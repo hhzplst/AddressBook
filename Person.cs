9146d828f0cfaddc92b98963a18f716902f2de7f
Ivy Chang <ivycwq@icloud.com>
2016-09-15 09:58:16 -0700

    moving ToString method out

--- Person.cs
+++ Person.cs
@@ -14,16 +14,6 @@ namespace AddressBook {
     public string GetFullName() {
       return FirstName + " " + LastName;
     }
-    public override string ToString() {
-      return String.Format("**********************************************************\n" +
-                           "{0, -10} {1, -15} {2, -15} {3, -20}\n" +
-                           "**********************************************************\n" + 
-                           "{4, -10} {5, -15} {6, -15} {7, -20}\n", "ID", "FIRSTNAME", "LASTNAME", "FULLNAME", 
-                           ID, FirstName, LastName, GetFullName());
-    }
-    public void PrintInfo() {
-      Console.WriteLine(this);
-    }
     private static int SetID() {
       return ++LastIDAssigned;
     }
